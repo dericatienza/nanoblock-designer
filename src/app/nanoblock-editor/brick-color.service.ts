feafd61ab1f3b7a7dd1c6d58dd8a9d4328d27faa
Deric Atienza <dericatienza@live.com.ph>
2019-01-17 17:05:57 +0800

    Inconsistent brick detection when selecting/painting/deleting

--- src/app/nanoblock-editor/brick-color.service.ts
+++ src/app/nanoblock-editor/brick-color.service.ts
@@ -11,58 +11,59 @@ export const DEFAULT_BRICK_COLOR_HEX = '#FFFFFF';
 
 @Injectable()
 export class BrickColorService {
-  brickColorsUrl = 'assets/default-brick-colors.json';
+    brickColorsUrl = 'assets/default-brick-colors.json';
 
-  brickColorMaterials: Map<number, MeshPhongMaterial>;
+    brickColorMaterials: Map<number, MeshPhongMaterial>;
 
-  constructor(private _http: HttpClient) {
-    this.brickColorMaterials = new Map<number, MeshPhongMaterial>();
-  }
-
-  getDefaultBrickColors() {
-    return this._http.get<BrickColor[]>(this.brickColorsUrl);
-  }
-
-  clearBrickColorMaterials() {
-    this.brickColorMaterials.clear();
-  }
-
-  getBrickColorMaterial(brickColor: BrickColor): Material {
-    if (this.brickColorMaterials.has(brickColor.id)) {
-      return this.brickColorMaterials.get(brickColor.id);
+    constructor(private _http: HttpClient) {
+        this.brickColorMaterials = new Map<number, MeshPhongMaterial>();
     }
 
-    const opacity = brickColor.isClear ? CLEAR_COLOR_OPACITY : 1;
-
-    const material = new MeshPhongMaterial(
-      {
-        color: brickColor.colorHex,
-        opacity: opacity,
-        transparent: brickColor.isClear,
-        polygonOffset: true,
-        polygonOffsetFactor: 1,
-        polygonOffsetUnits: 1
-      }
-    );
-
-    this.brickColorMaterials.set(brickColor.id, material);
-
-    return material;
-  }
-
-  updateBrickColorMaterial(brickColor: BrickColor) {
-    const material = this.brickColorMaterials.get(brickColor.id);
-
-    if (material) {
-      material.color.set(brickColor.colorHex);
-      material.opacity = brickColor.isClear ? CLEAR_COLOR_OPACITY : 1;
-      material.transparent = brickColor.isClear;
+    getDefaultBrickColors() {
+        return this._http.get<BrickColor[]>(this.brickColorsUrl);
     }
-  }
 
-  deleteBrickColorMaterial(brickColor: BrickColor) {
-    if (this.brickColorMaterials.has(brickColor.id)) {
-      return this.brickColorMaterials.delete(brickColor.id);
+    clearBrickColorMaterials() {
+        this.brickColorMaterials.clear();
     }
-  }
+
+    getBrickColorMaterial(brickColor: BrickColor): Material {
+        if (this.brickColorMaterials.has(brickColor.id)) {
+            return this.brickColorMaterials.get(brickColor.id);
+        }
+
+        const opacity = brickColor.isClear ? CLEAR_COLOR_OPACITY : 1;
+
+        const material = new MeshPhongMaterial(
+            {
+                color: brickColor.colorHex,
+                opacity: opacity,
+                transparent: brickColor.isClear,
+                polygonOffset: true,
+                polygonOffsetFactor: 1,
+                polygonOffsetUnits: 1,
+                side: THREE.DoubleSide
+            }
+        );
+
+        this.brickColorMaterials.set(brickColor.id, material);
+
+        return material;
+    }
+
+    updateBrickColorMaterial(brickColor: BrickColor) {
+        const material = this.brickColorMaterials.get(brickColor.id);
+
+        if (material) {
+            material.color.set(brickColor.colorHex);
+            material.opacity = brickColor.isClear ? CLEAR_COLOR_OPACITY : 1;
+            material.transparent = brickColor.isClear;
+        }
+    }
+
+    deleteBrickColorMaterial(brickColor: BrickColor) {
+        if (this.brickColorMaterials.has(brickColor.id)) {
+            return this.brickColorMaterials.delete(brickColor.id);
+        }
+    }
 }
