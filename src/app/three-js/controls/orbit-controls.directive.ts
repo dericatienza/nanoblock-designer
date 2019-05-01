8e644d96293c09ad68e6fa89ef4da35c3349ad64
Deric Atienza <dericatienza@live.com.ph>
2019-05-01 17:56:55 +0800

    Set camera to pan in screen space

--- src/app/three-js/controls/orbit-controls.directive.ts
+++ src/app/three-js/controls/orbit-controls.directive.ts
@@ -7,43 +7,44 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 declare var THREE: any;
 
 @Directive({
-  selector: 'three-orbit-controls',
-  exportAs: 'three-orbit-controls'
+    selector: 'three-orbit-controls',
+    exportAs: 'three-orbit-controls'
 })
 export class OrbitControlsDirective implements AfterViewInit {
 
-  @ContentChildren(AbstractCamera, { descendants: true }) childCameras: QueryList<AbstractCamera<three.Camera>>;
+    @ContentChildren(AbstractCamera, { descendants: true }) childCameras: QueryList<AbstractCamera<three.Camera>>;
 
-  @Input() rotateSpeed = 1.0;
-  @Input() zoomSpeed = 1.2;
-  @Input() renderer: RendererComponent;
+    @Input() rotateSpeed = 1.0;
+    @Input() zoomSpeed = 1.2;
+    @Input() renderer: RendererComponent;
 
-  private _controls: OrbitControls;
+    private _controls: OrbitControls;
 
-  get controls(): OrbitControls {
-    return this._controls;
-  }
-
-  constructor(private _elemRef: ElementRef) {
-  }
-
-  ngAfterViewInit(): void {
-    if (this.childCameras === undefined || this.childCameras.first === undefined) {
-      throw new Error('Camera is not found');
+    get controls(): OrbitControls {
+        return this._controls;
     }
 
-    this._controls = new OrbitControls(this.childCameras.first.camera, this.renderer.canvas);
-    this._controls.rotateSpeed = this.rotateSpeed;
-    this._controls.zoomSpeed = this.zoomSpeed;
-    // this._controls.addEventListener('change', this.childRenderers.first.render);
-    // this.childRenderers.first.render();
-  }
+    constructor(private _elemRef: ElementRef) {
+    }
 
-  // enable() {
-  //   this._controls.enabled = true;
-  // }
+    ngAfterViewInit(): void {
+        if (this.childCameras === undefined || this.childCameras.first === undefined) {
+            throw new Error('Camera is not found');
+        }
 
-  // disable() {
-  //   this._controls.enabled = false;
-  // }
+        this._controls = new OrbitControls(this.childCameras.first.camera, this.renderer.canvas);
+        this._controls.screenSpacePanning = true;
+        this._controls.rotateSpeed = this.rotateSpeed;
+        this._controls.zoomSpeed = this.zoomSpeed;
+        // this._controls.addEventListener('change', this.childRenderers.first.render);
+        // this.childRenderers.first.render();
+    }
+
+    // enable() {
+    //   this._controls.enabled = true;
+    // }
+
+    // disable() {
+    //   this._controls.enabled = false;
+    // }
 }
